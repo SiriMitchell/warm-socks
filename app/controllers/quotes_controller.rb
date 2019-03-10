59448cc7d2f7f17af2f37fe28b4957cccdddc167
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-03-10 20:15:25 +0000

    worked on error msg. doesnt work yet

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,6 +10,8 @@ class QuotesController < ApplicationController
 
   def create
     Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Oh snap!</strong> You should check in on some of those fields below and try submitting again.'
     redirect_to root_path
   end
 
