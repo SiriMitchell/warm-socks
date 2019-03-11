d2c777e877ceea46d6597ff22cdd60e468e39453
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-03-11 00:34:34 +0000

    fixed some issues

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,16 +3,19 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-
-  def new
+   def new
     @quote = Quote.new
   end
 
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
     if @quote.invalid?
       flash[:error] = '<strong>Oh snap!</strong> You should check in on some of those fields below and try submitting again.'
+    end
     redirect_to root_path
+  end
+
+  def about
   end
 
   private
@@ -20,5 +23,4 @@ class QuotesController < ApplicationController
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
-
 end
